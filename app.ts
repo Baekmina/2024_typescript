4adec014287dccabc929431fb499d2509bbc2d23
mina <koyoa12@naver.com>
2024-01-03 23:09:51 +0900

    object type

--- app.ts
+++ app.ts
@@ -1,20 +1,12 @@
-// 매개변수에 타입 할당하기 (값+콜론+타입) => : number & : string & : boolean
-function add(n1: number, n2: number, showResult: boolean, parse: string) {
-  // if (typeof n1 !== "number" || typeof n2 !== "number") {
-  //   throw new Error("Incorrect input");
-  // }
-  const result = n1 + n2;
-  if (showResult) {
-    console.log(parse + result);
-  } else {
-    return result;
-  }
-}
+// object
 
-const number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-const resultParse = "Result is: ";
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+const person = {
+  name: "Maximilian",
+  age: 30,
+};
 
-const result = add(number1, number2, printResult, resultParse);
-// console.log(result);
+console.log(person.name);
---
+++ basics.ts
@@ -0,0 +1,20 @@
+// 매개변수에 타입 할당하기 (값+콜론+타입) => : number & : string & : boolean
+function add(n1: number, n2: number, showResult: boolean, parse: string) {
+  // if (typeof n1 !== "number" || typeof n2 !== "number") {
+  //   throw new Error("Incorrect input");
+  // }
+  const result = n1 + n2;
+  if (showResult) {
+    console.log(parse + result);
+  } else {
+    return result;
+  }
+}
+
+const number1 = 5;
+const number2 = 2.8;
+const printResult = true;
+const resultParse = "Result is: ";
+
+const result = add(number1, number2, printResult, resultParse);
+// console.log(result);
